00b1471d2b1e0a2e71907fb5218c4894f25c751d
yoons <kys5037k@naver.com>
2022-08-07 01:44:44 +0900

    test

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
-    params:{},
+    params:{
+        api_key:'a6a0c307c2ee20d7ab006b8e2cd8c398',
+        language:"ko-KR",
+    },
 });
+
+export default instance;
