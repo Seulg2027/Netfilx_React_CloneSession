3bebf4c49ff1de5b58cae4fc1a61d386add61431
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-08-08 16:22:59 +0900

    8/10 세션 search, detail, router

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,36 +1,40 @@
-import "./Nav.css";
+import './Nav.css';
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Nav() {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if(window.scrollY > 50) {
-                setShow(true);
-            } else{
-                setShow(false);
-            }
-        })
-        return () => {
-            window.addEventListener("scroll", () => {});
-        }
-    }, []);
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
 
-    return (
-        <nav className={`nav ${show && "nav_black"}`}>
-            <img
-                alt="Netflix logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2880px-Netflix_2015_logo.svg.png"
-                className='nav__logo'
-                onClick={()=>window.location.reload()}
-            />
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.addEventListener('scroll', () => {});
+    };
+  }, []);
 
-            <img
-                alt="User logged"
-                src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
-                className='nav__avater'
-            />
-        </nav>
-    )
+  return (
+    <nav className={`nav ${show && 'nav_black'}`}>
+      <img
+        alt="Netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2880px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <input value={searchValue} onChange={handleChange} className="nav__input" type="text" placeholder="영화 제목을 입력해주세요." />
+      <img alt="User logged" src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg" className="nav__avater" />
+    </nav>
+  );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -3,6 +3,14 @@ import { useEffect, useState } from 'react';
 import './Row.css';
 import MovieModal from './MovieModal/index';
 
+// swiper //
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Scrollbar, A11y } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 function Row({ title, id, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
@@ -27,27 +35,54 @@ function Row({ title, id, fetchUrl, isLargeRow }) {
     <section className="row">
       <h2>{title}</h2>
       {/* 슬라이더 */}
-      <div className="slider">
-        <div className="slider__arrow-left">
+      <Swiper
+        className="slider"
+        scrollbar={{ draggable: true }}
+        modules={[Navigation, Scrollbar, A11y]}
+        navigation
+        loop={true} //끝에 갔을때 돌아오기
+        breakpoints={{
+          //크기별로 다르게 주기
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
+        {/* <div className="slider__arrow-left">
           <span className="arrow">{'<'}</span>
-        </div>
+        </div> */}
         {/* 영화 여러 개를 key 값을 이용해 반복문 돌리기 */}
         <div id={id} className="row__posters">
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
-              src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-              loading="lazy"
-              alt={movie.name}
-              onClick={() => handleClick(movie)}
-            />
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                loading="lazy"
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div className="slider__arrow-right">
+        {/* <div className="slider__arrow-right">
           <span className="arrow">{'>'}</span>
-        </div>
-      </div>
+        </div> */}
+      </Swiper>
       {modalOpen && <MovieModal {...movieSelected} setModalOpen={setModalOpen} />}
     </section>
   );
